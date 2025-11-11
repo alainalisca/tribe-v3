590b77b8d5941019bc5f32a0830c541d7a319683
alainalisca <98769249+alainalisca@users.noreply.github.com>
2025-11-11 15:34:03 -0500

    Add iOS Safari compatibility meta tags

--- tribe-v3/app/layout.tsx
+++ tribe-v3/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="theme-color" content="#C0E863" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="format-detection" content="telephone=no" />
       </head>
       <body>
         <LanguageProvider>
